23819eb7415fc0b26ab037c28edab273447592ab
mageramaksim1-dev <mageramaksim1@gmail.com>
2026-08-09 13:59:38 +0300

    Delete Нова папка (3)/LoveTatianka-main/src/main/java/hw/zako/zakohealthindicator/client/mixin/ImGameGudMixin.java

--- Нова папка (3)/LoveTatianka-main/src/main/java/hw/zako/zakohealthindicator/client/mixin/ImGameGudMixin.java
+++
@@ -1,9 +0,0 @@
-package hw.zako.zakohealthindicator.client.mixin;
-
-/**
- * Legacy mixin placeholder. HUD rendering is implemented with Fabric's 26.2 Hud API.
- */
-public final class ImGameGudMixin {
-    private ImGameGudMixin() {
-    }
-}
